a13b6dd409ee78c9ec004504fe8de320b9c7f0ba
stephy <stephshiro35@gmail.com>
2024-03-24 23:57:05 +0300

    add

--- challenge2/index.js
+++ challenge2/index.js
@@ -3,20 +3,15 @@ function checkSpeed() {
     let carSpeed = document.querySelector("#carSpeed").value;
 
 
-
-
-
-
     console.log(carSpeed);
 
 
-
     let result = "OK";
-
-
+//Any speed below 70 is considerd ok
 
     if (carSpeed > 70) {
 
+        //Every 5 km/s above the speed limit (70),gives one demerit point 
         let demeritPoints = (carSpeed - 70) / 5;
 
 
