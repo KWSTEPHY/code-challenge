9ffe647d4b68bad18bfe0748411f3bc3302b3276
stephy <stephshiro35@gmail.com>
2024-03-24 22:25:28 +0300

    add

---
+++ challenge3/index.js
@@ -0,0 +1,85 @@
+function calcSalary() {
+
+    let basicSalary = document.querySelector("#basicSalary").value;
+    let benefits = document.querySelector("#benefits").value;
+
+    console.log(basicSalary);
+    console.log(benefits);
+
+
+    let taxableIncome = (basicSalary - benefits);
+
+
+    console.log(taxableIncome);
+
+    if (taxableIncome < 24000) {
+
+        tax = "0%";
+    } else {
+
+        if (taxableIncome >= 120000) {
+
+            tax = (taxableIncome*0.3);
+
+        }
+        else if (taxableIncome >= 80000) {
+
+            tax = (taxableIncome*0.25);
+
+        }
+        else if (taxableIncome >= 40000) {
+
+            tax = (taxableIncome*0.2);
+
+        }
+        else if (taxableIncome >= 24000) {
+
+            tax = (taxableIncome*0.15);
+
+        }
+        else {
+
+            tax = (taxableIncome*0.1);
+        }
+
+
+
+    }
+    console.log(tax);
+
+    if (taxableIncome <= 5999) {
+        nhifDeduction = 150;
+
+    } else if (taxableIncome <= 7999) {
+        nhifDeduction = 300;
+
+    } else if (taxableIncome <= 11999) {
+        nhifDeduction = 400;
+
+    } else if (taxableIncome <= 14999) {
+        nhifDeduction = 500;
+
+    } else if (taxableIncome <= 19999) {
+        nhifDeduction = 600;
+
+    } else if (taxableIncome <= 24999) {
+        nhifDeduction = 750;
+
+    } else if (taxableIncome <= 29999) {
+        nhifDeduction = 850;
+
+    } else if (taxableIncome <= 34999) {
+        nhifDeduction = 900;
+
+    } else if (taxableIncome <= 39999) {
+        nhifDeduction = 950;
+
+    } else {
+        nhifDeduction = 1000;
+    }
+    
+console.log(nhifDeduction);
+}
+
+
+
